f8e80918e29d15fe590aeae908b486a1034582a6
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2023-07-01 16:22:15 -0300

    Create remove-duplicates-from-sorted-array.java

---
+++ remove-duplicates-from-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        /* It's like a pointer for the unique elements. I set as 0 because the element at
+        the index 0 will be unique */
+
+        /* [0, 1, 1, 1, 2] 
+        *         ^---- unique pointer 
+        *               ^---- different from its previous 
+        * replace unique with the different one: [0, 1, 2, 1, 2] 
+        * the first 3 elemets are unique [0, 1, 2]
+        */
+
+        /* I don't have to worry with the 0 index element. When I first find an element
+        that is different from its previous I can modify the element where the pointer 'unique' is pointing*/
+
+        int unique = 1;
+
+        /* With -1 i assure that wont invalid access, cuz im going until that last but one
+        Other possible solution would be compare with its antecesor. i = 1 and nums[i] != nums[i - 1] */
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] != nums[i + 1]) {
+                nums[unique] = nums[i + 1];
+                unique++;
+            }
+        }
+
+        return unique;
+    }
+}
