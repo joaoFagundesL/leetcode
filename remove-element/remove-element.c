c9fd451f377182683d7ced54c7239e6bd3e25e9f
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2023-07-03 22:36:52 -0300

    Create remove-element.c

---
+++ remove-element/remove-element.c
@@ -0,0 +1,36 @@
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+int removeElement(int* nums, int numsSize, int val) {
+
+    if(numsSize == 0) return 0;
+
+    int i = 0, j = numsSize - 1;
+    int aux;
+
+    while(i != j) {
+        if(nums[i] == val) {
+            if(nums[j] == val)
+                j--;
+            else {
+                nums[i] = nums[j];
+                nums[j] = val;
+            }
+        } else
+            i++;
+    }
+
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[i] == val) {
+            aux = i;
+            break;
+        }
+    }
+
+    return aux;
+}
