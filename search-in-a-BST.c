65941efd2510fab4dba003263e8bde77b3f525c1
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2023-07-27 21:21:18 -0300

    Create search-in-a-BST.c

---
+++ search-in-a-BST.c
@@ -0,0 +1,12 @@
+struct TreeNode* searchBST(struct TreeNode* root, int val){
+    while(root != NULL) {
+        if(val < root->val) 
+            root = root->left;
+        else if(val > root->val)
+            root = root->right;
+        else
+            break;
+    }
+
+    return root;
+}
