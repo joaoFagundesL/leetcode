656b943f20a3feddec5ceccdb12330ae64bb8e64
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-01 17:03:18 -0300

    Time: 5 ms (33.80%), Space: 6 MB (98.59%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.c
@@ -0,0 +1,30 @@
+struct TreeNode **search(struct TreeNode **root, int val) {
+  struct TreeNode **p = root;
+  while(*p != NULL) {
+    if(val < (*p)->val)
+      p = &((*p)->left);
+    else
+      p = &((*p)->right);
+  }
+
+  return p;
+}
+
+void insert(struct TreeNode **root, int val) {
+  struct TreeNode *node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+  assert(node != NULL);
+  node->left = NULL;
+  node->right = NULL;
+  node->val = val;
+
+  struct TreeNode **parent = search(root, val);
+  *parent = node; 
+}
+
+struct TreeNode* bstFromPreorder(int* preorder, int preorderSize){
+  struct TreeNode *root = NULL;
+  for(int i = 0; i < preorderSize; i++) 
+    insert(&root, preorder[i]);
+
+  return root;
+}
