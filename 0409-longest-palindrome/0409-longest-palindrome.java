593b9ae51617bbd8412dc541affc7ac8510cad9b
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-02 18:03:33 -0300

    Time: 14 ms (5.19%), Space: 42.2 MB (15.84%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int len = s.length();
+        
+        if (len == 1)
+            return 1;
+        
+        HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
+        
+        for (char c : s.toCharArray()) {
+            if (!hash.containsKey(c))
+                hash.put(c, 1);
+            else
+                hash.put(c, hash.get(c) + 1);
+        }
+        
+        int ans = 0, odd = 0;
+        
+        for (Map.Entry<Character, Integer> entry : hash.entrySet()) {
+            int val = entry.getValue();
+            
+            System.out.println(val);
+            
+            if (val % 2 == 0)
+                ans += val;
+            else {
+                ans += val - 1; 
+                odd = 1;
+            }
+        }
+        
+        return ans + odd;
+    }
+}
