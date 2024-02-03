bbadbed874441e7e0a121e0db867bc9c5a2158af
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-03 16:45:01 -0300

    Time: 95 ms (48.57%), Space: 18.3 MB (44.29%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.py
+++ 0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -17,37 +17,20 @@ class Solution(object):
             x, h = building[0], building[1]
             
             if h < 0:
-                print("added to heap: ", h)
                 heapq.heappush(max_heap, h)
                 
                 if -max_heap[0] > max_height:
                     ans.append([x, -h])
                     max_height = -max_heap[0]
-                    print("updated max height: ", max_height)
             else:
-                
-                if h in removed:
-                    removed[h] += 1   
-                    print("incremented to", removed[h], h)
-                else:
-                    print("first time inserted: ", h)
-                    removed[h] = 1
+                removed[h] = removed.get(h, 0) + 1 # counting the frequency of the keys
                                
                 while -max_heap[0] in removed and removed[-max_heap[0]] > 0:
-                    print("-max_heap[0] is in removed", -max_heap[0])
-                    print("removing 1 from", -max_heap[0])
-                    
                     removed[-max_heap[0]] -= 1
                     heapq.heappop(max_heap)
                    
                 if -max_heap[0] != max_height:
-                    print("new high height", -max_heap[0])
                     ans.append([x, -max_heap[0]])
                     max_height = -max_heap[0]
-                
-                # print("AFTER")
-                # print("removed ", removed)
-                # print("heap ", max_heap)
-                # print("\n")
                     
         return ans
