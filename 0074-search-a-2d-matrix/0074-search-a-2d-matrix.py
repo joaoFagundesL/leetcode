642d1e5e006214ab3f19441a2bf4c5542c7b96e7
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-04 15:24:34 -0300

    Time: 19 ms (93.05%), Space: 11.9 MB (99.29%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -13,8 +13,14 @@ class Solution(object):
             else: 
                 return True
         
-        print(row_end)
-            
+        # if the value is not found in any of the rows, it means
+        # that the variable row_start holds the position where
+        # the value should be inserted to mantain the order, for example:
+        # [10, 20, 30, 40] => if i'm searching for 25, the row_start pointer
+        # will be at the position 2 (value 30), because it is where i have to insert it
+        # keeping the order. However, the row_end points to the position 1 (value 20), and
+        # that's the line i'm interested in, hence i use row_end to search in the col
+        
         while col_start <= col_end:
             mid_col = ((col_end - col_start) // 2) + col_start
                     
