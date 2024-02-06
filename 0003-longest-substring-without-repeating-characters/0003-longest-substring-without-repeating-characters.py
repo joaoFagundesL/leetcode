a3d08303b08adfc89d49cdc1782dff13b5c6fe32
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-06 20:10:39 -0300

    Time: 47 ms (92.12%), Space: 16.5 MB (90.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,21 +4,14 @@ class Solution:
         if len_s == 1: return 1
         if len_s == 0: return 0
         
-        curr, hash_set, m, start, end = 1, set(), 1, 0, 1
-        hash_set.add(s[0])
+        hash_set, ans, start, end = set(), 0, 0, 0
+       
         while end < len_s:
-            if s[end] in hash_set:
+            while s[end] in hash_set:
+                hash_set.remove(s[start])
                 start += 1
-                hash_set.clear()
-                hash_set.add(s[start])
-                end = start + 1
-                curr = 1
-            else:
-                hash_set.add(s[end])
-                curr += 1
-                end += 1
-                if curr > m:
-                    m = curr
-                    if m > end - start + 1:
-                        return m        
-        return m
+                
+            hash_set.add(s[end])
+            ans = max(ans, end - start + 1)
+            end += 1   
+        return ans
