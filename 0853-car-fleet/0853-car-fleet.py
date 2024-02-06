e695a65ed47331508cb182f37d718c0a4a73716c
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-06 01:49:13 -0300

    Time: 668 ms (84.95%), Space: 39.1 MB (50.19%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,10 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        highest_time, removed = -1, 0
+        removed = 0
         arr = [(position[i], speed[i]) for i in range(len(position))]
         arr.sort(reverse=True)
         
-        for p, s in arr:
+        highest_time = (target-arr[0][0])/arr[0][1]
+        
+        for p, s in arr[1:]:
             time = (target - p)/s # it needs to be float
             if time <= highest_time:
                 removed += 1
