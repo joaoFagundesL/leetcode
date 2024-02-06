34e14b577f89267247721a80a348c0a35b558804
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-06 00:51:54 -0300

    Time: 670 ms (83.64%), Space: 38.4 MB (63.21%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,15 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        curr, ans, stack, = 0, 0, []
+        ans, stack, = 1, []
         arr = [(position[i], speed[i]) for i in range(len(position))]
         arr.sort(reverse=True)
         
         for p, s in arr:
-            time = (target - p) / s # it needs to be float
-            
-            if curr < time:
-                ans += 1
-                curr = time
-                
-        return ans
+            stack.append((target - p) / s) # it needs to be float
+            if len(stack) >= 2 and stack[-1] <= stack[-2]: # -1 is the top of the stack
+                stack.pop()
+        return len(stack)
         
