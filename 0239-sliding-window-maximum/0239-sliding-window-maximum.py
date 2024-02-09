a1e80847fa8c702c017343d58e692823555e4f8d
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-08 21:08:33 -0300

    Time: 1244 ms (41.22%), Space: 36.2 MB (10.49%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,18 +3,18 @@ class Solution:
         nums_size, ans, max_heap = len(nums), [], []
         
         for idx in range(k):
-            heappush(max_heap, -1 * nums[idx])
+            heappush(max_heap, -nums[idx])
             
         removed = {}
-        ans.append(max_heap[0] * -1)
+        ans.append(-max_heap[0])
         
         for idx in range(k, nums_size):
             heappush(max_heap, -1 * nums[idx])
             removed[nums[idx - k]] = removed.get(nums[idx - k], 0) + 1
-            while max_heap[0] * -1 in removed and removed[max_heap[0] * -1] > 0:
-                removed[max_heap[0] * -1] -= 1
+            while -max_heap[0] in removed and removed[-max_heap[0]] > 0:
+                removed[-max_heap[0]] -= 1
                 heappop(max_heap)
-            ans.append(max_heap[0] * -1)
+            ans.append(-max_heap[0])
             
         return ans;
             
