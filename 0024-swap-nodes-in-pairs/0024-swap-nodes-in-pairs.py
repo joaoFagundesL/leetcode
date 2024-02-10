0bd65e08cc8e5715c2c5f16af1ac556330ff8ffb
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-09 21:58:43 -0300

    Time: 28 ms (94.34%), Space: 16.5 MB (66.31%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,6 +5,15 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # example [1,2,3,4,5,6]
+        #
+        # CALL STACK
+        #  x  y      
+        #  NULL => return NULL x
+        # [5->6] => invert [6->5] then return x
+        # [3->4] => invert [4->3] then get the return of the above call [4->3->6->5] 
+        # [1->2->3->4->5->6] invert [2->1] then get the return above [2->1->4->3->6->5]
+        
         if head is None or head.next is None:
             return head
         x = head
