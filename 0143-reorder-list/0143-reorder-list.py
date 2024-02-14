6a97232682d0005facdc77a8e26c3ae4497f1e80
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-14 15:43:40 -0300

    Time: 55 ms (68.71%), Space: 24.6 MB (93.98%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,20 +1,8 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        slow, fast = head, head
-        
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        
+        slow = self.getMiddle(head)
         second = slow
-        y, aux = second.next, second.next
-        
-        second.next = None
-        while y and aux:
-            aux = y.next
-            y.next = second
-            second = y
-            y = aux
+        second = self.reverseList(second)
       
         x, tmp1, tmp2 = head, head.next, second.next
         while tmp2 and second:
@@ -24,4 +12,20 @@ class Solution:
             second = tmp2
             tmp1 = tmp1.next
             tmp2 = tmp2.next
-        
+    
+    def getMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        y, aux = head.next, head.next
+        head.next = None
+        while y and aux:
+            aux = y.next
+            y.next = head
+            head = y
+            y = aux
+        return head
