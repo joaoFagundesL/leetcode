f593ff2103f9edcfc167d325495cfe45d27924d8
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-15 01:01:02 -0300

    Time: 250 ms (88.68%), Space: 63.4 MB (98.97%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,29 +1,34 @@
 class RandomizedSet:
+
     def __init__(self):
-        self.buckets = [[] for _ in range(16)] 
-        self.filled_buckets = {}
-        
+        self.my_hash = {}
+        self.my_list = []
+
     def insert(self, val: int) -> bool:
-        bucketIdx = val % 16
-        if val not in self.buckets[bucketIdx]:
-            self.buckets[bucketIdx].append(val)
-            self.filled_buckets[val] = self.filled_buckets.get(val, 0) + 1
+        if val not in self.my_hash:
+            self.my_hash[val] = len(self.my_list)
+            self.my_list.append(val)
             return True
-        else: 
-            return False
-        
+        return False
+
     def remove(self, val: int) -> bool:
-        bucketIdx = val % 16
-        if val in self.buckets[bucketIdx]:
-            self.buckets[bucketIdx].remove(val)
-            if val in self.filled_buckets:
-                self.filled_buckets[val] -= 1
-                if self.filled_buckets[val] == 0:
-                    del self.filled_buckets[val]
+        if val in self.my_hash:
+            idx = self.my_hash[val]
+            lastVal = self.my_list[-1]
+            self.my_list[idx] = lastVal
+            self.my_list.pop()
+            self.my_hash[lastVal] = idx # update the new index of the element that was swapped
+            del self.my_hash[val]
             return True
-        else:
-            return False
-        
+        return False
+
     def getRandom(self) -> int:
-        return random.choice(list(self.filled_buckets.keys()))
-       
+        return random.choice(self.my_list)
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
