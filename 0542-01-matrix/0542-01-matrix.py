c0d9a701c38e7e6fc3f0bb6737240ae424143a16
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-16 00:45:28 -0300

    Time: 434 ms (73.12%), Space: 19.7 MB (77.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,33 +1,28 @@
-class Solution:
-    def __init__(self):
-        self.deq = collections.deque()
-        self.num_row = 0
-        self.num_col = 0
-        self.directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        
+class Solution:    
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        self.num_row = len(mat)
-        self.num_col = len(mat[0])
-        ans = [[float('inf')] * self.num_col for _ in range(self.num_row)]
-        
-        for r in range(self.num_row):
-            for c in range(self.num_col):
-                if mat[r][c] == 0: # bfs from the zero
-                    ans[r][c] = 0
-                    self.deq.append((r, c))
-                    
-        while self.deq:  
-            row, col = self.deq.popleft()
-            for dr, dc in self.directions:
-                newRow = row + dr
-                newCol = col + dc
-                        
-                if (newRow < 0 or newCol < 0) or (newRow >= self.num_row or newCol >= self.num_col) or mat[newRow][newCol] == 0:
-                    continue
-                    
-                if ans[newRow][newCol] > ans[row][col] + 1:
-                    ans[newRow][newCol] = ans[row][col] + 1
-                    self.deq.append((newRow, newCol))
-                
-        return ans
-               
+      num_row = len(mat)
+      num_col = len(mat[0])
+      ans = [[float('inf')] * num_col for _ in range(num_row)]
+      directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+      deq = collections.deque()
+      
+      for r in range(num_row):
+        for c in range(num_col):
+          if mat[r][c] == 0: # bfs from the zero
+            ans[r][c] = 0
+            deq.append((r, c))
+
+      while deq:  
+        row, col = deq.popleft()
+        for dr, dc in directions:
+          newRow = row + dr
+          newCol = col + dc
+
+          if (newRow < 0 or newCol < 0) or (newRow >= num_row or newCol >= num_col) or mat[newRow][newCol] == 0:
+            continue
+
+          if ans[newRow][newCol] > ans[row][col] + 1:
+            ans[newRow][newCol] = ans[row][col] + 1
+            deq.append((newRow, newCol))
+
+      return ans
