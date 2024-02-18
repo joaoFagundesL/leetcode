08b840501c42981adbd8d1397b102c6a5a423098
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-18 00:21:18 -0300

    Time: 54 ms (74.70%), Space: 22 MB (53.20%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -1,16 +1,25 @@
+# USING BFS
+# class Solution:
+#     def countNodes(self, root: Optional[TreeNode]) -> int:
+#       if root is None: return 0
+#       deq = collections.deque([root])
+#       count = 0
+#       while deq:
+#         len_deq = len(deq)
+#         for _ in range(len_deq):
+#           n = deq.popleft()
+#           count += 1
+#           if n.left:
+#             deq.append(n.left)
+#           if n.right:
+#             deq.append(n.right)
+#       return count
+        
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
       if root is None: return 0
-      deq = collections.deque([root])
-      count = 0
-      while deq:
-        len_deq = len(deq)
-        for _ in range(len_deq):
-          n = deq.popleft()
-          count += 1
-          if n.left:
-            deq.append(n.left)
-          if n.right:
-            deq.append(n.right)
-      return count
+      l = self.countNodes(root.left)
+      r = self.countNodes(root.right)
+      return l + r + 1
+
         
