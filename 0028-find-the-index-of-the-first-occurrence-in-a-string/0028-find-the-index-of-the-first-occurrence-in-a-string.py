424c3a8750f95ca285ee72f6ee4ebf7f370550c1
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-18 22:48:02 -0300

    Time: 41 ms (24.55%), Space: 16.5 MB (70.74%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
       len_haystack, len_needle = len(haystack), len(needle)
-     
-      for idx in range(len_haystack):
-        if len_haystack - idx < len_needle:
-          return -1
-        else:
-          for j in range(len_haystack):
-            # idx + j means i'm going forward in the haystack because idx only iterates one time
-            # for all values of j
-            if haystack[idx + j] != needle[j]:
-              break
-            if j == len_needle - 1:
-              return idx
+      # sadbutsad => no need to start the index from index 7 because the len of
+      # needle would be greater and i wont find waht im looking for
+      valid_len = len_haystack + 1 - len_needle
+      
+      for idx in range(valid_len):
+        for j in range(len_haystack):
+          # idx + j means i'm going forward in the haystack because idx only iterates one time
+          # for all values of j
+          if haystack[idx + j] != needle[j]:
+            break
+          if j == len_needle - 1:
+            return idx
       return -1
