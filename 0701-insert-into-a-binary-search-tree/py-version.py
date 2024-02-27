e4a92ecd5ae185a5b3391d4fc3ff0bc5d806016d
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-02-27 17:56:40 -0300

    Update py-version.py

--- 0701-insert-into-a-binary-search-tree/py-version.py
+++ 0701-insert-into-a-binary-search-tree/py-version.py
@@ -1,22 +1,33 @@
+# class Solution:
+#     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         parent = self.search(root, val)
+#         node = TreeNode(val)
+#         if not parent:
+#             return node
+#         if node.val < parent.val:
+#             parent.left = node
+#         else:
+#             parent.right = node
+#         return root
+
+#     def search(self, root: Optional[TreeNode], val: int):
+#         parent = root
+#         aux = None
+#         while parent:
+#             aux = parent
+#             if val < parent.val:
+#                 parent = parent.left
+#             elif val > parent.val:
+#                 parent = parent.right
+#         return aux
+
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        parent = self.search(root, val)
-        node = TreeNode(val)
-        if not parent:
-            return node
-        if node.val < parent.val:
-            parent.left = node
-        else:
-            parent.right = node
+        if root is None:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
         return root
-
-    def search(self, root: Optional[TreeNode], val: int):
-        parent = root
-        aux = None
-        while parent:
-            aux = parent
-            if val < parent.val:
-                parent = parent.left
-            elif val > parent.val:
-                parent = parent.right
-        return aux
+            
