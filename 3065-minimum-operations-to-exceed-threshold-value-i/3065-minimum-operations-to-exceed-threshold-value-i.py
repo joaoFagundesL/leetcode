d2160c25596572a66dff87778e577057b8e1462a
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-03-04 13:49:42 -0300

    Time: 50 ms (100.00%), Space: 16.6 MB (100.00%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        heap = []
+        
+        for e in nums:
+            heapq.heappush(heap, e)
+        
+        ans = 0
+        for i in range(n):
+            if heap[0] < k:
+                heapq.heappop(heap)
+                ans += 1
+            else:
+                return ans
