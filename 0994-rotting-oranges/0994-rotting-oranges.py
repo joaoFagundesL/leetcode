12b051281fb66b58c882a02f181f109e5d97db3a
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-03-07 20:14:03 -0300

    Time: 50 ms (61.71%), Space: 16.5 MB (96.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,41 +1,37 @@
-class Solution:
-    def __init__(self):
-        self.directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        self.deq = collections.deque()
-        self.num_row = 0
-        self.num_col = 0
-        self.visited = set()
-        
+class Solution:        
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        self.num_row, self.num_col = len(grid), len(grid[0])
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        deq = collections.deque()
+        visited = set()
+        num_row, num_col = len(grid), len(grid[0])
         ans, fresh = 0, 0
         
-        for i in range(self.num_row):
-            for j in range(self.num_col):
+        for i in range(num_row):
+            for j in range(num_col):
                 if grid[i][j] == 2:
-                    self.deq.append((i, j, 0))
-                    self.visited.add((i, j))
+                    deq.append((i, j, 0))
+                    visited.add((i, j))
                 elif grid[i][j] == 1:
                     fresh += 1
                 
-        while self.deq:
-            row, col, time = self.deq.popleft()
+        while deq:
+            row, col, time = deq.popleft()
             ans = max(ans, time)
-            for dr, dc in self.directions:
+            for dr, dc in directions:
                 newRow, newCol = row + dr, col + dc
                 
                 if (
-                    (newRow < 0 or newRow >= self.num_row) 
-                    or (newCol < 0 or newCol >= self.num_col)
-                    or (newRow, newCol) in self.visited 
+                    (newRow < 0 or newRow >= num_row) 
+                    or (newCol < 0 or newCol >= num_col)
+                    or (newRow, newCol) in visited 
                     or grid[newRow][newCol] == 0
                 ): continue
                 
                 fresh -= 1
                 grid[newRow][newCol] = 2
-                self.deq.append((newRow, newCol, time + 1))
-                self.visited.add((newRow, newCol))
-        
+                deq.append((newRow, newCol, time + 1))
+                visited.add((newRow, newCol))
+                
         return -1 if fresh > 0 else ans
                 
                 
