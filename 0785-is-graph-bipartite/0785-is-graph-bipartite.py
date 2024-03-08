a6313a24de0490c40970c8bd31b983007dd4ab91
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-03-08 20:46:04 -0300

    Time: 146 ms (60.03%), Space: 17.2 MB (42.52%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,35 +1,33 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n, m = len(graph), len(graph[0])
-        adj = {i: [] for i in range(n)}
+        # -1 = not visited, 0 and 1 are the groups
         arr = [-1] * n
-
-        for i in range(n):
-            for neighbor in graph[i]:
-                adj[i].append(neighbor)
-
+        
+        # handling the case if not all vertex are reachable
         for i in range(n):
             if arr[i] == -1:
-                if self.bfs(i, adj, arr) == False:
+                if self.bfs(i, graph, arr) == False:
                     return False
         return True
 
     def bfs(self, start, adj, arr) -> bool:
         deq = collections.deque()
 
-        arr[start] = 1
+        arr[start] = 0
         deq.append(start)
 
         while deq:
             node = deq.popleft()
             for nei in adj[node]:
-                if arr[nei] != -1:
-                    if arr[nei] == arr[node]:
+                if arr[nei] != -1: # not visited
+                    if arr[nei] == arr[node]: # same group
                         return False
                     continue
+                # setting valid groups for unvisited nodes
                 elif arr[node] == 1:
-                    arr[nei] = 2
-                elif arr[node] == 2:
-                    arr[nei] = 1
+                    arr[nei] = arr[node] & 0
+                elif arr[node] == 0:
+                    arr[nei] = arr[node] ^ 1
                 deq.append(nei)
         return True
