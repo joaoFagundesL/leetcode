4c865b0a73a6663afe01a92a2e52fc66829a5639
Joao Fagundes <93099304+joaoFagundesL@users.noreply.github.com>
2024-03-12 15:02:14 -0300

    Time: 0 ms (100.00%), Space: 9.6 MB (88.43%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void backtracking(vector<vector<int>>& ans, int freq[], vector<int>& p, vector<int>& nums, int n, set<vector<int>>& hash) {
+    // O(n!)
+    void backtracking(vector<vector<int>>& ans, int freq[], vector<int>& p, vector<int>& nums, int n) {
       
-      if(p.size() == n && hash.find(p) == hash.end()) {
+      if(p.size() == n) {
         ans.push_back(p);
-        hash.insert(p);
         return;
       }
       
       for(int i = 0; i < nums.size(); i++) {
-        if(freq[i] == 1) 
+        if(freq[i] == 1 || (i > 0 && nums[i] == nums[i - 1] && freq[i - 1] == 0))
           continue;
         freq[i] = 1;
         p.push_back(nums[i]);
-        backtracking(ans, freq, p, nums, n, hash);
+        backtracking(ans, freq, p, nums, n);
         freq[i] = 0;
         p.pop_back(); 
-        
       }
     }
     
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
       int n = nums.size();
       int freq[n];
       for(int i = 0; i < n; i++)
@@ -28,10 +28,7 @@ public:
       
       vector<vector<int>> ans;
       vector<int> p;
-      set<vector<int>> hash;
-      
-      backtracking(ans, freq, p, nums, n, hash);
-      
+      backtracking(ans, freq, p, nums, n);
       
       return ans;
      }
